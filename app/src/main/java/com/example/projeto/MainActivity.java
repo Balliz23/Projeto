76de36173acc007381b4631f22ae8b294fc96080
Balliz <pietrotile@gmail.com>
2023-04-20 14:22:45 -0300

    Terminado -O cálculo para conseguir a diferença de datas foi implementado. -A diferença de datas será mostrada em um TextView.

--- app/src/main/java/com/example/projeto/MainActivity.java
+++ app/src/main/java/com/example/projeto/MainActivity.java
@@ -23,39 +23,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    
+        //Conexao dos elementos xml com o código Java
         etData = findViewById(R.id.etData);
         btnProsseguir = findViewById(R.id.btnProsseguir);
-        etData.setFocusable(false);
-        etData.setKeyListener(null);
 
-        Calendar calendar = Calendar.getInstance();
+        etData.setFocusable(false); // Não atrapalhar no processo de seleção da data
+        etData.setKeyListener(null); //Não abrir o teclado quando o campo for selecionado
+
+        Calendar calendar = Calendar.getInstance(); // Seleciona o calendário a ser usado de acordo com o fusohorário por meio dojava runtime environment
+        // Recebe os valores atuais para abrir o seletor da data no dia atual
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        etData.setOnClickListener(new View.OnClickListener() {
+        etData.setOnClickListener(new View.OnClickListener() { // O campo de texto é interativo
             @Override
             public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                DatePickerDialog datePickerDialog = new DatePickerDialog( //Este componente permite o uso de uma interface gráfica para a seleção da data
                         MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
+                    @Override //modifica o componente original
                     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                         month = month+1;
-                        String data = day+"/"+month+"/"+year;
-                        etData.setText(data);
+                        String data = day+"/"+month+"/"+year; // Organização da data de modo Brasileiro
+                        etData.setText(data); // Definir o texto do et como a data selecionada
                     }
                 },year,month,day);
-                datePickerDialog.show();
+                datePickerDialog.show(); // Abre o componente modificado
             }
         });
         btnProsseguir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            if(!etData.getText().toString().isEmpty()) {
+            if(!etData.getText().toString().isEmpty()) { //não prosseguir caso o campo de data estiver vazio
                 Intent i1 = new Intent(MainActivity.this, Tempo_Vivo.class);
                 i1.putExtra("data", etData.getText().toString());
-                startActivity(i1);
+                startActivity(i1); //mudar de tela e levar a informação da data para outra tela
             }
             }
         });
--- app/src/main/java/com/example/projeto/Tempo_Vivo.java
+++ app/src/main/java/com/example/projeto/Tempo_Vivo.java
@@ -23,24 +23,26 @@ public class Tempo_Vivo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tempo_vivo);
+        //Conexao dos elementos xml com o código Java
         btnRetornar = findViewById(R.id.btnRetornar);
         tvTempo_Vivo = findViewById(R.id.tvTempo_Vivo);
         tvData = findViewById(R.id.tvData);
 
+        //Resgata os valores da tela anterior e os armazenam em novas variaveis
         Bundle extras = getIntent().getExtras();
         String data_nascimento = extras.getString("data");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-        LocalDate localDate = LocalDate.parse(data_nascimento, formatter);
-        LocalDate todayBrasil = LocalDate.now(ZoneId.of("America/Sao_Paulo"));
-        tvData.setText("Data de nascimento: "+data_nascimento);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");//Função usada para formatar a string em um formato de data desejado
+        LocalDate localDate = LocalDate.parse(data_nascimento, formatter); //Formatar a String em formato de date data
+        LocalDate todayBrasil = LocalDate.now(ZoneId.of("America/Sao_Paulo"));// Resgata a data real em São Paulo/ América
+        tvData.setText("Data de nascimento: "+data_nascimento); // Set o texto no tv para a data de nascimento previamente solicitada
 
-        int anos_gap = Integer.parseInt(String.valueOf(ChronoUnit.YEARS.between(localDate, todayBrasil)));
-        int meses_gap = Integer.parseInt(String.valueOf(ChronoUnit.MONTHS.between(localDate, todayBrasil)))%12;
-        int dias_gap = Integer.parseInt(String.valueOf(ChronoUnit.DAYS.between(localDate, todayBrasil)))%30;
+        int anos_gap = Integer.parseInt(String.valueOf(ChronoUnit.YEARS.between(localDate, todayBrasil))); //Função para achar anos entre datas
+        int meses_gap = Integer.parseInt(String.valueOf(ChronoUnit.MONTHS.between(localDate, todayBrasil)))%12; //Função para achar meses entre datas, com % eliminando os meses já listados nos anos
+        int dias_gap = Integer.parseInt(String.valueOf(ChronoUnit.DAYS.between(localDate, todayBrasil)))%30; //Função para achar dias entre datas, com % eliminando os dias já listados
         if (anos_gap < 0 || meses_gap < 0 || dias_gap < 0){
-            tvTempo_Vivo.setText("Parece que temos um viajante do tempo entre nós!!!");
+            tvTempo_Vivo.setText("Parece que temos um viajante do tempo entre nós!!!"); //se o usuário inserir uma data futura uma mensagem diferente aparecera
         }else{
-            tvTempo_Vivo.setText(anos_gap+" anos, "+meses_gap+" meses e "+dias_gap+" dias.");
+            tvTempo_Vivo.setText(anos_gap+" anos, "+meses_gap+" meses e "+dias_gap+" dias."); // Mostrar o tempo vivido
         }
         btnRetornar.setOnClickListener(new View.OnClickListener() {
             @Override
